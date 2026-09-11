89d486c47cc45c137e472316bf533a0b78bac9e6
Anshu Sahani <anshusahani@Anshus-MacBook-Air.local>
2026-09-11 19:02:05 +0530

    Folder Structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+
+# creating files 
+
+touch src/__init__.py
+touch src/helper.py
+touch .env
+touch src/prompt.py
+touch src/app.py
+touch src/setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and Files created successfully"
